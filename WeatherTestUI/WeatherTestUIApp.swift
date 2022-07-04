74b91f8f7375c431f20a2291ac353c8fac8a9589
jackvdp <jack@pumpymusic.co.uk>
2022-07-04 14:00:43 +0100

    Initial Commit

---
+++ WeatherTestUI/WeatherTestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestUIApp.swift
+//  WeatherTestUI
+//
+//  Created by Jack Vanderpump on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
